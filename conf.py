ae7b5529f0deab13e314d7e725d538a2c2f6e699
deg <degurni@rambler.ru>
2023-11-02 01:33:36 +0300

    ok

--- conf.py
+++ conf.py
@@ -3,7 +3,9 @@
 key = '37183f91-bb5e-43c7-ab98-979474209be9'
 secret = '5AD20C1E094508003865FAA2B1B18E8D'
 passw = '#-ffD7GnC*eL9k~'
-
+# key = '85b0ed78-6a11-4d89-9a6d-2f475eb2f923'
+# secret = '5415362D70CB3E4BE202C7D3F0D75AD1'
+# passw = '#-ffD7GnC*eL9k~'
 
 symbols = ['LINK-USDT-SWAP', 'TRB-USDT-SWAP', 'DYDX-USDT-SWAP']
 
