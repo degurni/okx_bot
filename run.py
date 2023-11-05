941ac306ae4f2cb048c1a3f40ea636abc7b96f42
degurni <degurni@rambler.ru>
2023-11-05 09:25:14 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,8 +2,17 @@ import time
 
 import conf
 import classes
+from classes import Bot
 
+bot = Bot()
 
+def start():
+    bot.debug('debug', 'Бот запущен на бирже OKX ')
+    bot.debug('debug', f'Отслеживаем {len(conf.symbols)} торговых пар')
+    for symbol in conf.symbols:
+        print(symbol.center(30, '_'))
+    bot.chek_files()
+    bot.checking_open_positions()
 
 def search():
     for symbol in conf.symbols:
@@ -11,7 +20,7 @@ def search():
         df = classes.frame(data=data)
         df = classes.add_indicator(df=df)
         if df.SIG.iloc[-1] != 0:
-            print(f'{df.index[-1]}:Получен сигнал --> {symbol}: {df.SIG.iloc[-1]}')
+            print(f'Получен сигнал --> {symbol}: {df.SIG.iloc[-1]}')
 
 
 
@@ -26,9 +35,15 @@ def search():
 
 
 if __name__ == '__main__':
-    classes.chek_files()
-    while True:
-        search()
-        time.sleep(conf.sleep_1)
+    try:
+        start()
+
+        while True:
+            search()
+            time.sleep(conf.sleep_1)
+    except KeyboardInterrupt:
+        bot.debug('debug', 'Бот остановлен вручную')
+
+
 
 
